3ae57ed0a43a936258e0bbcd414b4fc5605b770a
EshaBachuwar <eshabachuwar09@gmail.com>
2024-03-13 15:20:40 +0530

    Express setup

--- api/index.js
+++ api/index.js
@@ -1,2 +1,26 @@
-require("dotenv").config()
-require("./connection")
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+const app = express();
+
+require('dotenv').config();
+require('./connection');
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/user",userRoute)
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || 'something went wrong';
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
---
+++ api/routes/user.js
@@ -0,0 +1,10 @@
+import express from 'express'
+
+const router=express.Router()
+
+//get 
+
+
+//post
+
+export default router 
