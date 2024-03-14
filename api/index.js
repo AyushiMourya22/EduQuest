64fb85bb8761cad9e364905622254f8d0a333eeb
AyushiMourya22 <ayushimourya42@gmail.com>
2024-03-15 00:21:21 +0530

    minor changes

--- api/index.js
+++ api/index.js
@@ -17,13 +17,8 @@ app.use(cors());
 app.use('/api/user', userRoute);
 app.use('/api/auth', authRoute);
 
-app.use((err, req, res, next) => {
-  const errorStatus = err.status || 500;
-  const errorMessage = err.message || 'something went wrong';
-  return res.status(errorStatus).json({
-    success: false,
-    status: errorStatus,
-    message: errorMessage,
-    stack: err.stack,
-  });
-});
+
+
+app.listen(5000,()=>{
+  console.log("Listening at port 5000")
+})
