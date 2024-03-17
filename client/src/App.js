2704aa656395726de558ce2abd44c36ddf657174
AyushiMourya22 <ayushimourya42@gmail.com>
2024-03-17 18:06:14 +0530

    Added login and register page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
-
+import './'
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Login from "./pages/Login";
+import Register from './pages/Register';
 function App() {
   return (
-    <h1>
-      Hello world
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/components/AuthNavbar.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AuthNavbar() {
+    return (
+        <div className='font-bold text-[#888043] px-2 text-4xl py-4 md:px-7 '>EduQuest</div>
+    )
+}
